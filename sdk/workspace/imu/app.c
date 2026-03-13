11e4418c77ac38e5d459fe26f9ae5b6fae078fe7
Makoto Shimojima <shimojima_makoto@nias.ac.jp>
2026-03-13 18:20:13 +0900

    Update SPIKE API for IMU; calibration data is not loaded yet, but otherwise it looks good.

--- sdk/workspace/imu/app.c
+++ sdk/workspace/imu/app.c
@@ -5,7 +5,7 @@
 
 #include <spike/hub/imu.h>
 #include <pbio/imu.h>
-#include <pbdrv/imu.h>
+#include <math.h>
 
 /* メインタスク(起動時にのみ関数コールされる) */
 void main_task(intptr_t unused) {
@@ -16,28 +16,20 @@ void main_task(intptr_t unused) {
   fprintf(fp, "Start !!\n");
     
   hub_imu_init();
-  pbio_imu_persistent_settings_t settings;
-  pbio_imu_set_default_settings(&settings);
 
+  // HackSPi's hub is tilted at ~51 degrees.
+  hub_imu_set_tilt(51.0f);
+
+  // Show angular velocity, acceleration, heading
   while (1) {
-    pbdrv_imu_dev_t *imu_dev;
-    pbdrv_imu_config_t *imu_config;
-    pbio_error_t err = pbdrv_imu_get_imu(&imu_dev, &imu_config);
-    if (err != PBIO_SUCCESS) fprintf(fp, "imu_dev: %d\n", err);
-    bool ready = pbio_imu_is_ready();
-    bool pbio_q = pbio_imu_is_stationary();
-    bool pbdrv_q = pbdrv_imu_is_stationary(imu_dev);
-    fprintf(fp, "[%c%c%c] ", ready ? 'T' : 'F', pbio_q ? 'T' : 'F', pbdrv_q ? 'T' : 'F');
-    float v[3],  a[3], t;
-    int  iv[3], ia[3];
-    hub_imu_get_angular_velocity(v);
+    float w[3], a[3];
+    bool stationary = hub_imu_is_stationary();
+    fprintf(fp, "[%c] ", stationary ? 'T' : 'F');
     hub_imu_get_acceleration(a);
-    t = hub_imu_get_temperature();
-    for (int i = 0; i < 3; i++) {
-      iv[i] = (int) v[i];
-      ia[i] = (int) a[i];
-    }
-    fprintf(fp, "v = (%d %d %d), a = (%d %d %d), t = %f\n", iv[0], iv[1], iv[2], ia[0], ia[1], ia[2], t);
+    fprintf(fp, "a = (%.0f %.0f %.0f); ", a[0], a[1], a[2]);
+    hub_imu_get_angular_velocity(w);
+    fprintf(fp, "ω = (%.0f %.0f %.0f); ", w[0], w[1], w[2]);
+    fprintf(fp, "h = %.0f\n", hub_imu_get_heading());
     dly_tsk(1000*1000);
   }
   
